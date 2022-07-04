662e9766f35b86de254a248bd58f8cbb96fa3fee
Michael Tyson <myty@users.noreply.github.com>
2022-07-04 13:31:48 -0400

    dotnet minimal api

---
+++ apps/api/GraphQLBuilderExtensions.cs
@@ -0,0 +1,34 @@
+using GraphQL.DI;
+
+namespace GraphQL.Examples.Todo;
+
+public static class GraphQLBuilderExtensions
+{
+    public static IGraphQLBuilder AddUnhandledExceptionHandler(this IGraphQLBuilder builder)
+    {
+        return builder.ConfigureExecutionOptions(options =>
+        {
+            var logger = options.RequestServices.GetRequiredService<ILogger<Program>>();
+            options.UnhandledExceptionDelegate = ctx =>
+            {
+                logger.UnhandledException(ctx.OriginalException.Message);
+                return Task.CompletedTask;
+            };
+        });
+    }
+}
+
+internal static class LoggerExtensions
+{
+    private static readonly Action<ILogger, string, Exception> _unhandledException =
+        LoggerMessage.Define<string>(
+            LogLevel.Error,
+            new EventId(1, nameof(UnhandledException)),
+            "{Error} occurred"
+        );
+
+    public static void UnhandledException(this ILogger logger, string message)
+    {
+        _unhandledException(logger, message, null);
+    }
+}
---
+++ apps/api/Program.cs
@@ -0,0 +1,48 @@
+using GraphQL;
+using GraphQL.DataLoader;
+using GraphQL.Examples.Todo;
+using GraphQL.MicrosoftDI;
+using GraphQL.Relay.Types;
+using GraphQL.Server;
+using GraphQL.SystemTextJson;
+using GraphQL.Todo.Core.Interfaces;
+using GraphQL.Todo.Schema;
+using GraphQL.Todo.Services.Services;
+using GraphQL.Types.Relay;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ITodoService, TodoService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddTransient(typeof(ConnectionType<>));
+builder.Services.AddTransient(typeof(EdgeType<>));
+builder.Services.AddTransient<NodeInterface>();
+builder.Services.AddTransient<PageInfoType>();
+
+builder.Services.AddGraphQL(
+    graphQLBuilder =>
+        graphQLBuilder
+            .AddHttpMiddleware<TodoSchema>()
+            .AddWebSocketsHttpMiddleware<TodoSchema>()
+            .AddSchema<TodoSchema>()
+            .AddUnhandledExceptionHandler()
+            .AddSystemTextJson()
+            .AddWebSockets()
+            .AddDataLoader()
+            .AddGraphTypes(typeof(TodoSchema).Assembly)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseWebSockets();
+app.UseGraphQLWebSockets<TodoSchema>();
+app.UseGraphQL<TodoSchema>();
+app.UseGraphQLAltair();
+app.Run();
