03eb345593ff4b6c3efd77dcca47f710527b8d58
Michael Tyson <myty@users.noreply.github.com>
2022-07-16 18:24:31 -0400

    seed test todos

--- apps/api/ApplicationBuilderExtensions.cs
+++ apps/api/ApplicationBuilderExtensions.cs
@@ -14,9 +14,38 @@ public static class ApplicationBuilderExtensions
 
         var testUser = new User { Name = "Test User", Username = "test-user" };
 
-        if (context.Users.FirstOrDefault(user => user.Username == testUser.Username) == null)
+        var foundUser = context.Users.FirstOrDefault(user => user.Username == testUser.Username);
+        if (foundUser == null)
         {
-            _ = context.Users.Add(testUser);
+            var addedUser = context.Users.Add(testUser);
+
+            foundUser = addedUser.Entity;
+        }
+
+        Console.WriteLine("----  USER ----");
+        Console.WriteLine(foundUser.Id);
+
+        var todos = new List<Todo>
+        {
+            new Todo { UserId = foundUser.Id, Text = "Test Text #1" },
+            new Todo { UserId = foundUser.Id, Text = "Test Text #2" },
+            new Todo { UserId = foundUser.Id, Text = "Test Text #3" },
+            new Todo { UserId = foundUser.Id, Text = "Test Text #4" },
+            new Todo
+            {
+                UserId = foundUser.Id,
+                Text = "Test Text #4 (Completeted)",
+                Completed = true
+            },
+        };
+
+        foreach (var testTodo in todos)
+        {
+            var foundTodo = context.Todos.FirstOrDefault(todo => todo.Text == testTodo.Text);
+            if (foundTodo == null)
+            {
+                _ = context.Todos.Add(testTodo);
+            }
         }
 
         _ = context.SaveChanges();
